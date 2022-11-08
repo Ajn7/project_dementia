27b2f73a48dae9d3045b9cc70919496c4d8408b4
Ajn7 <arjuntrcr7@gmail.com>
2022-11-08 18:17:35 +0530

    added logout option

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 from user_app import models #importing token signals
 from user_app.api.serializers import RegistrationSerializer
+
+@api_view(['post'])
+def logout_view(request):
+    
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    
+    
 @api_view(['POST'])
 def registration_view(request):
     
